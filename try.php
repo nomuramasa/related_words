e8053fa2112eea68b74f02988379f0a53b2fd5b6
nommy <nomuramasam@gmail.com>
2019-03-20 17:05:58 +0900

    上手く番号帰らない

--- try.php
+++ try.php
@@ -11,33 +11,17 @@ $url = 'https://api.apitore.com/api/40/wordnet-simple/all?access_token=c3eeb546-
 $data = file_get_contents($url);
 $data = json_decode($data,true); // trueで、stdClassをArrayにできる
 
-$entries = $data['entries']; // 必要なデータセット
+$entries = $data['entries']; // 必要なデータセット（〇〇語のブロックが５個ぐらい）
 
+getIndexNum($entries);
 
-
-// echo '<pre>'; var_dump($data['entries']); echo '</pre>';
-// echo '<pre>'; var_dump($data->entries[2]->words); echo '</pre>';
-// $words = $data['entries'][2]['words']; // labeljaが'下位語'の番号が[2]なのか[1]なのか知りたい
-// echo '<pre>'; var_dump($words); echo '</pre>';
-
-echo '<hr>';
-
-// var_dump(array_search('同義語',$data['entries']));
-
-foreach($entries as $index => $assoc){
-
-	if(array_search('同義語',$assoc)){ 
-		return $index; // インデックス番号を返す
+function getIndexNum($blocks){
+	foreach($blocks as $index => $block){ // ブロック（〇〇語）ごとに順番に見ていく
+		if(array_search('同義語',$block)){ 
+			return $index; // インデックス番号を返す
+		}
 	}
-	// $labelja = array_search('同義語',$index);
-	// $index = array_search($labelja, $indexata['entries']);
-	// var_dump($index);
-
-	echo '<pre>'; var_dump($assoc); echo '</pre>'; 
-	if($index['labelja'] == '同義語'){
-		echo '同義語';
-	} 
-	echo '<hr>';
 }
+
 
 ?>
