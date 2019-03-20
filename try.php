c7d53b5e3d2d14a2080c6a13eb6bd64ea7e9149f
nommy <nomuramasam@gmail.com>
2019-03-20 17:00:52 +0900

    Word.Netから、同義語のブロックがあるインデックス番号を出力できた

--- try.php
+++ try.php
@@ -1,3 +1,4 @@
+<meta charset='utf8'> 
 <?php
 
 $word = 'プログラマ';
@@ -8,11 +9,35 @@ $url = 'https://api.apitore.com/api/40/wordnet-simple/all?access_token=c3eeb546-
 
 
 $data = file_get_contents($url);
-$data = json_decode($data);
+$data = json_decode($data,true); // trueで、stdClassをArrayにできる
 
-echo '<pre>'; var_dump($data->entries); echo '</pre>';
+$entries = $data['entries']; // 必要なデータセット
+
+
+
+// echo '<pre>'; var_dump($data['entries']); echo '</pre>';
 // echo '<pre>'; var_dump($data->entries[2]->words); echo '</pre>';
-// $words = $data->entries[2]->words;
+// $words = $data['entries'][2]['words']; // labeljaが'下位語'の番号が[2]なのか[1]なのか知りたい
 // echo '<pre>'; var_dump($words); echo '</pre>';
+
+echo '<hr>';
+
+// var_dump(array_search('同義語',$data['entries']));
+
+foreach($entries as $index => $assoc){
+
+	if(array_search('同義語',$assoc)){ 
+		return $index; // インデックス番号を返す
+	}
+	// $labelja = array_search('同義語',$index);
+	// $index = array_search($labelja, $indexata['entries']);
+	// var_dump($index);
+
+	echo '<pre>'; var_dump($assoc); echo '</pre>'; 
+	if($index['labelja'] == '同義語'){
+		echo '同義語';
+	} 
+	echo '<hr>';
+}
 
 ?>
