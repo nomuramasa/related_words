d2a55b7de849cf00b3fadc591e0763647a72fdda
nommy <nomuramasam@gmail.com>
2019-03-20 21:26:08 +0900

    訂正箇所を星マーク

--- index.php
+++ index.php
@@ -9,105 +9,110 @@ $times = $_GET['level']; // 第何階層までか
 
 <div class='container'>
 
-	<?php echoWords($input_word, 0, $times); // 関数「echoWords」を実行 ?>
+	<?php //☆if($_GET['approach'] == 'google' || $_GET['approach'] == 'yahoo'): // GoogleとYahooの時だけ ?>
+
+		<?php echoWords($input_word, 0, $times); // 関数「echoWords」を実行 ?>
 
 
-	<?php function echoWords($_word, $count, $times){ // 関数 ?>
-		
-		<?php
-		$count++; // カウンタ増やす
-
-		$word = str_replace(' ', '%20', $_word); // 半角スペースだとエラーになるので%20に直す
-		$word = urlencode($word);
-
-		sleep($_GET['rest']); // 数秒休憩 
-		
-		if($_GET['approach'] == 'google'){
-			// Googleの関連キーワード
-			$url = 'https://www.google.com/search?q='.$word; // Google検索のurl　半角スペースが入るとエラー
-			$html = file_get_contents($url); // htmlを取得 
-			// var_dump($url); echo '<br>'; var_dump($html); // 確認用
-			$rel_words_obj = phpQuery::newDocument($html)->find('table tr td p.aw5cc'); // 第一階層で関係する単語達のオブジェクトをスクレイピング
-
-		}else if($_GET['approach'] == 'yahoo'){
-			// Yahooのサジェストキーワード
-			$url = 'http://ff.search.yahoo.com/gossip?output=json&command='.$word; // キーワードAPI
-			$json_data = file_get_contents($url); // htmlを取得 
-			// var_dump($url); echo '<br>'; var_dump($json_data); // 上手くいかない時の確認用
-			$data_array = json_decode($json_data, true); // 連想配列にデコード
-			$rel_words_obj = $data_array['gossip']['results']; // 第一階層で関係する単語達のオブジェクトをスクレイピング
-
-		}else if($_GET['approach'] == 'synonym'){
-			// 類語
-			$url = 'https://api.apitore.com/api/40/wordnet-simple/all?access_token=c3eeb546-506f-4d4f-9f47-019f9bc2e761&word='.$word.'&pos=n%2Cv%2Ca%2Cr'; 
-			$data = file_get_contents($url);
-			$data = json_decode($data);
-			$rel_words_obj = $data->entries[2]->words;
-		}
-
-		?>
-
-		<div class='row mx-0'>
-
-			<?php
-
-			foreach($rel_words_obj as $rel_word_obj): 
+		<?php function echoWords($_word, $count, $times){ // 関数 ?>
 			
-				if($_GET['approach'] == 'google'){
-					// Googleの関連キーワード
-					$rel_word = $rel_word_obj->textContent; 
+			<?php
+			$count++; // カウンタ増やす
 
-				}else if($_GET['approach'] == 'yahoo') {
-					// Yahooのサジェストキーワード 
-					$rel_word = $rel_word_obj['key']; // テキストのみ 
+			$word = str_replace(' ', '%20', $_word); // 半角スペースだとエラーになるので%20に直す
+			$word = urlencode($word);
 
-				}else if($_GET['approach'] == 'synonym'){
-					// 類語
-					$rel_word = $rel_word_obj; 
-				}
+			sleep($_GET['rest']); // ☆数秒休憩 
+			
+			if($_GET['approach'] == 'google'){
+				// Googleの関連キーワード
+				$url = 'https://www.google.com/search?q='.$word; // Google検索のurl　半角スペースが入るとエラー
+				$html = file_get_contents($url); // htmlを取得 
+				// var_dump($url); echo '<br>'; var_dump($html); // 確認用
+				$rel_words_obj = phpQuery::newDocument($html)->find('table tr td p.aw5cc'); // 第一階層で関係する単語達のオブジェクトをスクレイピング
+
+			}else if($_GET['approach'] == 'yahoo'){
+				// Yahooのサジェストキーワード
+				$url = 'http://ff.search.yahoo.com/gossip?output=json&command='.$word; // キーワードAPI
+				$json_data = file_get_contents($url); // htmlを取得 
+				// var_dump($url); echo '<br>'; var_dump($json_data); // 上手くいかない時の確認用
+				$data_array = json_decode($json_data, true); // 連想配列にデコード
+				$rel_words_obj = $data_array['gossip']['results']; // 第一階層で関係する単語達のオブジェクトをスクレイピング
+
+			}else if($_GET['approach'] == 'synonym'){
+				// 類語
+				$url = 'https://api.apitore.com/api/40/wordnet-simple/all?access_token=c3eeb546-506f-4d4f-9f47-019f9bc2e761&word='.$word.'&pos=n%2Cv%2Ca%2Cr'; 
+				$data = file_get_contents($url);
+				$data = json_decode($data);
+				$rel_words_obj = $data->entries[2]->words;
+			}
 
 			?>
 
-				<?php if($count == 1): ?>
-					<div class='col-12 px-0'> <!-- 第1階層のボタンの外枠 -->
-					<a href='https://www.google.com/search?q=<?php echo $rel_word; ?>' target='_blank' class='btn btn-light m-2 border first'> <!-- 第1階層のボタン -->
-					<?php echo $rel_word; // 単語出力 ?>
-				<?php endif; ?>
+			<div class='row mx-0'>
 
-				<?php if($count == 2): ?> 
-					<object>
-					<a href='https://www.google.com/search?q=<?php echo $rel_word; ?>' target='_blank' class='btn btn-white m-2 text-dark second'> <!-- 第2階層のボタン -->
-					<?php echo $rel_word; // 単語出力 ?>
-				<?php endif; ?>
+				<?php
 
-				<?php if($count == 3): ?> 
-					<object>
-					<a href='https://www.google.com/search?q=<?php echo $rel_word; ?>' target='_blank' class='btn btn-light m-1 text-dark border third'> <!-- 第3階層のボタン -->
-					<?php echo $rel_word; // 単語出力 ?>
-				<?php endif; ?>
-				<?php if($count < $times): // まだ全回数終わってなければ更に繰り返す ?>
-					<?php echoWords($rel_word, $count, $times); // $rel_wordは一新、$countは増えてる、$timesはそのまま ?>
-				<?php endif; ?>
+				foreach($rel_words_obj as $rel_word_obj): 
+				
+					if($_GET['approach'] == 'google'){
+						// Googleの関連キーワード
+						$rel_word = $rel_word_obj->textContent; 
 
-				<?php if($count == 3): ?> 
-					</a>
-					</object>
-				<?php endif; ?>
+					}else if($_GET['approach'] == 'yahoo') {
+						// Yahooのサジェストキーワード 
+						$rel_word = $rel_word_obj['key']; // テキストのみ 
 
-				<?php if($count == 2): ?> 
-					</a>
-					</object>
-				<?php endif; ?>
+					}else if($_GET['approach'] == 'synonym'){
+						// 類語
+						$rel_word = $rel_word_obj; 
+					}
 
-				<?php if($count == 1): ?>
-					</a>
-					</div>
-				<?php endif; ?>
+				?>
 
-			<?php endforeach; ?>
-		</div> <!-- row -->
-	<?php } ?> <!-- 関数終わり -->
+					<?php if($count == 1): ?>
+						<div class='col-12 px-0'> <!-- 第1階層のボタンの外枠 -->
+						<a href='https://www.google.com/search?q=<?php echo $rel_word; ?>' target='_blank' class='btn btn-light m-2 border first'> <!-- 第1階層のボタン -->
+						<?php echo $rel_word; // 単語出力 ?>
+					<?php endif; ?>
 
+					<?php if($count == 2): ?> 
+						<object>
+						<a href='https://www.google.com/search?q=<?php echo $rel_word; ?>' target='_blank' class='btn btn-white m-2 text-dark second'> <!-- 第2階層のボタン -->
+						<?php echo $rel_word; // 単語出力 ?>
+					<?php endif; ?>
+
+					<?php if($count == 3): ?> 
+						<object>
+						<a href='https://www.google.com/search?q=<?php echo $rel_word; ?>' target='_blank' class='btn btn-light m-1 text-dark border third'> <!-- 第3階層のボタン -->
+						<?php echo $rel_word; // 単語出力 ?>
+					<?php endif; ?>
+
+					<?php if($count < $times): // まだ全回数終わってなければ更に繰り返す ?>
+						<?php // ☆sleep($_GET['rest']); // 数秒休憩はここに書けば、１回目は即実行で2回目以降の直前に挟むので、 ?>
+						<?php echoWords($rel_word, $count, $times); // $rel_wordは一新、$countは増えてる、$timesはそのまま ?>
+					<?php endif; ?>
+
+					<?php if($count == 3): ?> 
+						</a>
+						</object>
+					<?php endif; ?>
+
+					<?php if($count == 2): ?> 
+						</a>
+						</object>
+					<?php endif; ?>
+
+					<?php if($count == 1): ?>
+						</a>
+						</div>
+					<?php endif; ?>
+
+				<?php endforeach; ?>
+			</div> <!-- row -->
+		<?php } ?> <!-- 関数終わり -->
+
+	<?php // ☆endif; // GoogleとYahooの時だけ ?>
 </div> <!-- container -->
 
 <?php
--- try.php
+++ try.php
@@ -3,33 +3,39 @@
 
 $word = 'プログラマ';
 
-$word_code = urlencode($word);
+$word = urlencode($word);
 
-$url = 'https://api.apitore.com/api/40/wordnet-simple/all?access_token=c3eeb546-506f-4d4f-9f47-019f9bc2e761&word='.$word_code.'&pos=n%2Cv%2Ca%2Cr';
+sleep(3); // 3秒休憩 
 
+$url = 'https://api.apitore.com/api/40/wordnet-simple/all?access_token=c3eeb546-506f-4d4f-9f47-019f9bc2e761&word='.$word.'&pos=n%2Cv%2Ca%2Cr';
 
 $data = file_get_contents($url);
 $data = json_decode($data,true); // trueで、stdClassをArrayにできる
 
 $entries = $data['entries']; // 必要なデータセット（〇〇語のブロックが５個ぐらい）
 
-
-echo getBlockIndexNum('同義語',$entries); // 同義語のブロック番号を出力
-echo getBlockIndexNum('上位語',$entries); // 上位語のブロック番号を出力
-echo getBlockIndexNum('下位語',$entries); // 下位語のブロック番号を出力
-echo getBlockIndexNum('被包含領域',$entries); // 被包含領域語のブロック番号を出力
-
+// echo '<pre>'; var_dump($entries);echo '</pre>';  // 全部出力してみる
 
 echo '<hr>';
-$num = getBlockIndexNum('上位語',$entries); // 好きな種類を入れる
-$sym = $entries[$num]['words']; // その番号の単語達
-echo '<pre>'; var_dump($sym); echo '</pre>';  // 上位語のブロックをダンプ
+
+$synonym = getBlockWords('同義語', $entries); 
+$broader = getBlockWords('上位語', $entries); 
+$narrower = getBlockWords('下位語', $entries);
+
+$component = getBlockWords('構成要素', $entries); 
+$inclusion = getBlockWords('被包含領域', $entries); 
 
 
-function getBlockIndexNum($kind, $blocks){ // 〇〇語のブロック番号を返す関数
+echo '<pre>'; var_dump($broader); echo '</pre>';  // 欲しいブロックをダンプ
+
+foreach($broader as $word){
+
+}
+
+function getBlockWords($kind, $blocks){ // 〇〇語のブロックの単語達を返す関数
 	foreach($blocks as $index => $block){ // ブロック（〇〇語）ごとに順番に見ていく
-		if(array_search($kind, $block)){ 
-			return $index; // インデックス番号を返す
+		if(array_search($kind, $block)){ // 欲しい要素の時に
+			return $blocks[$index]['words']; // インデックス番号を当てはめて、その情報ブロックの単語達を返す
 		}
 	}
 }
