850abb1293cae49b4e951824d57a9a2c12e0d3b7
野村 昌史 <nomuramasam@gmail.com>
2023-07-06 16:57:15 +0900

    トップページの関連ワード出力構造を3階層から2階層に変更

--- index.php
+++ index.php
@@ -11,32 +11,24 @@
 
   <div class='container'>
     <div class='row mx-0'>
-      <div class='col-12 px-0'>
-        <!-- 第1階層のボタン -->
-        <a class='btn btn-light m-2 border first'>
-          第１関連ワード
-          <div class='row mx-0'>
-            <?php for ($i = 1; $i <= 3; $i++) : ?>
-              <object>
-                <!-- 第2階層のボタン -->
-                <a class='btn btn-white m-2 text-dark second'>
-                  第２関連ワード
-                  <div class='row mx-0'>
-                    <?php for ($k = 1; $k <= 8; $k++) : ?>
-                      <object>
-                        <!-- 第3階層のボタン -->
-                        <a class='btn btn-light m-1 text-dark border third'>
-                          第３関連ワード
-                        </a>
-                      </object>
-                    <?php endfor; ?>
-                  </div>
-                </a>
-              </object>
-            <?php endfor; ?>
-          </div>
-        </a>
-      </div>
+      <?php for ($i = 1; $i <= 3; $i++) : ?>
+        <div class='col-12 px-0'>
+          <!-- 第1階層のボタン -->
+          <a class='btn btn-light m-2 border first'>
+            第１関連ワード
+            <div class='row mx-0'>
+              <?php for ($k = 1; $k <= 15; $k++) : ?>
+                <object>
+                  <!-- 第2階層のボタン -->
+                  <a class='btn btn-white m-2 text-dark second'>
+                    第２関連ワード
+                  </a>
+                </object>
+              <?php endfor; ?>
+            </div>
+          </a>
+        </div>
+      <?php endfor; ?>
     </div>
   </div>
 </body>
